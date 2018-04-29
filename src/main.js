4915ce9bc7f926f521449d22452fb0552cc8a059
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-04-29 12:39:03 +0700

    Update control

--- src/main.js
+++ src/main.js
@@ -9,19 +9,29 @@ new Vue({
     music: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/270473197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
   },
   methods: {
-    setRadio: function(url) {
-      this.radio = url  
+    setRadio: function(url, btn) {
+      this.radio = url
+      var police = document.getElementById('police')
+      for (var i = 0; i < police.childNodes.length; i++) {
+        police.childNodes[i].className = 'btn'
+      }
+      btn.currentTarget.classList.add('active')
     },
-    setMusic: function(url) {
-      this.music = url  
+    setMusic: function(url, btn) {
+      this.music = url
+      var police = document.getElementById('sc')
+      for (var i = 0; i < police.childNodes.length; i++) {
+        police.childNodes[i].className = 'btn'
+      }
+      btn.currentTarget.classList.add('active')
     },
     play: function(){
       widget1.play()
-      document.getElementById('music').play()
+      document.querySelector('audio').play()
     },
     pause: function(){
       widget1.pause()
-      document.getElementById('music').pause()
+      document.querySelector('audio').pause()
     },
     next: function(){
       widget1.next()
@@ -29,7 +39,10 @@ new Vue({
   }
 })
 
-var iframeElement = document.querySelector('iframe');
-var widget1 = SC.Widget(iframeElement);
-widget1.play();
-widget1.setVolume(50);
+var iframeElement = document.querySelector('iframe')
+var widget1 = SC.Widget(iframeElement)
+widget1.play()
+widget1.setVolume(40)
+
+var audioElement = document.querySelector('audio')
+audioElement.volume = 1
