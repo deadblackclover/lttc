5ab0852d2a6170ce8c0d2fa096c425c1d59454c4
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-04-29 19:18:40 +0700

    Add volume music and radio

--- src/main.js
+++ src/main.js
@@ -1,29 +1,39 @@
 import Vue from 'vue/dist/vue.js'
 import '../src/main.styl'
 import '../src/api.js'
+import urlRadio from '../src/urlr.js'
+import urlMusic from '../src/urlm.js'
 
 new Vue({
   el: '#app',
   data: {
     radio: 'http://stream.scansf.com:8000/scansf.hp',
-    music: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/270473197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+    music: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/270473197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    volR: 50,
+    volM: 50
   },
   methods: {
-    setRadio: function(url, btn) {
-      this.radio = url
+    setRadio: function(name, btn) {
+      this.radio = urlRadio[name]
       var police = document.getElementById('police')
       for (var i = 0; i < police.childNodes.length; i++) {
         police.childNodes[i].className = 'btn'
       }
       btn.currentTarget.classList.add('active')
     },
-    setMusic: function(url, btn) {
-      this.music = url
-      var police = document.getElementById('sc')
-      for (var i = 0; i < police.childNodes.length; i++) {
-        police.childNodes[i].className = 'btn'
+    setMusic: function(name, btn) {
+      this.music = urlMusic[name]
+      var sc = document.getElementById('sc')
+      for (var i = 0; i < sc.childNodes.length; i++) {
+        sc.childNodes[i].className = 'btn'
       }
       btn.currentTarget.classList.add('active')
+    },
+    setVolR: function() {
+      audioElement.volume = this.volR/100
+    },
+    setVolM: function() {
+      widget1.setVolume(this.volM)
     },
     play: function(){
       widget1.play()
---
+++ src/urlr.js
@@ -0,0 +1,5 @@
+export default {
+  'SFPD1' : 'http://stream.scansf.com:8000/scansf.996xt',
+  'SFPD2' : 'http://stream.scansf.com:8000/scansf.hp',
+  'SFFD' : 'http://stream.scansf.com:8000/scansf.sffd',
+}
