b30bdebca9e2cdbf70ab6a948d925a5be51b39f0
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-06-02 22:44:08 +0700

    Fix url radio

--- src/urlr.js
+++ src/urlr.js
@@ -1,9 +1,5 @@
 export default {
-  'SFPD1' : 'http://stream.scansf.com:8000/scansf.996xt',
-  'SFPD2' : 'http://stream.scansf.com:8000/scansf.hp',
-  'SFFD' : 'http://stream.scansf.com:8000/scansf.sffd',
-  'Baltimore' : 'http://relay.broadcastify.com:80/3kjgws1bfp2nmy0',
-  'NewYork' : 'http://relay.broadcastify.com:80/fpbzrmd5xqy3267',
-  'Safety1': 'http://listen.broadcastify.com:80?t=A24E0ED02C4ABF8E48B6C69AAA1E26C93D8E6BA5D9759F74ABE8B76E752AED650D5650DE18FCE7D42745730C76C943F76275C993B5B755C5B7DD6F184CFE7B9F',
-  'Safety2' : 'http://listen.broadcastify.com:80?t=E9F1B855A0CDACEF5777264A4C71E86AE39DED15E8C8DC6A3BE40F85BA9C871FE78F5F8271F5136098F8AD292B73546C'
+  'SFPD1' : 'https://stream.scansf.com:8000/scansf.996xt',
+  'SFPD2' : 'https://stream.scansf.com:8000/scansf.hp',
+  'SFFD' : 'https://stream.scansf.com:8000/scansf.sffd'
 }
