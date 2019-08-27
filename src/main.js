a670b846fd44c1ec8abf9b8e0fb04b6ff9223580
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-08-27 21:01:13 +0700

    Added adaptive design

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ new Vue({
   el: '#app',
   data: {
     radio: 'http://stream.scansf.com:8000/scansf.hp',
-    music: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/270473197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    music: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/300494469&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
     volR: 50,
     volM: 50
   },
--- src/urlm.js
+++ src/urlm.js
@@ -1,5 +1,4 @@
 export default {
   'lofi' : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/300494469&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
-  'chill' : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/270473197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
   'trance' : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/205604793&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
 }
